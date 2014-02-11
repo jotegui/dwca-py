3032cbcdd686f2549443f3b1f9097bf2a7ad304c
Javier Otegui <javier.otegui@gmail.com>
2014-02-10 17:04:10 -0700

    Clean-up

--- completeness.py
+++
@@ -1,8 +0,0 @@
-def termCompleteness(dwca):
-    completeness = {}
-    total = dwca.countRecords()
-    for term in dwca.populatedTerms:
-        compl = total - dwca.countTermValue(term, "")
-        perc = "{0}%".format(round(compl*100.0/total,2))
-        completeness[term] = {'total': compl, 'percentage': perc}
-    return completeness
